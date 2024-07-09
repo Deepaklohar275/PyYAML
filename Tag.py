af8fdd48b31e7e30bab34b31660aad1b561d77dd
Deepak lohar <Deepaklohar275@gmail.com>
2024-07-10 01:01:51 +0530

    Adding YAML

---
+++ Tag.py
@@ -0,0 +1,49 @@
+import yaml
+import requests
+tags = []
+def imagetag (url):
+
+    r= requests.get(url, headers=headers)
+    response_dict = r.json()
+    image_dicts = response_dict['results']
+    image_dict= image_dicts[0]
+    for image_dict in image_dicts:
+        #print(image_dict['name'])
+        tags.append(image_dict['name'])
+    return tags
+url = "https://hub.docker.com/v2/repositories/deepaklohar/snyk/tags"
+headers = {
+    'Accept': 'application/json',  # Specify that you want a JSON response
+}
+tags =imagetag(url)
+#save to YAML as a Data Variable
+data = {'imageTags': tags}
+#print(data)
+print(tags)
+with open('Tags.yaml', 'w') as file:
+    yaml.dump(data, file, default_flow_style=False)
+
+    
+    
+
+    
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
